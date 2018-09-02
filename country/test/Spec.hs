7996b152657e7965d654bc8128d8e1ea65f17d36
Andrew Martin <andrew.thaddeus@gmail.com>
2018-09-01 20:02:02 -0400

    add some tests for country

--- country/test/Spec.hs
+++ country/test/Spec.hs
@@ -1,2 +1,23 @@
+import Country (Country)
+import Data.Proxy (Proxy(..))
+import Test.Tasty (defaultMain,testGroup,TestTree)
+
+import qualified Test.QuickCheck.Classes as QCC
+import qualified Test.QuickCheck.Classes.IsList as QCCL
+import qualified Test.Tasty.QuickCheck as TQC
+import qualified Test.QuickCheck as QC
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain $ testGroup "Country"
+  [ lawsToTest (QCC.eqLaws (Proxy :: Proxy Country))
+  , lawsToTest (QCC.ordLaws (Proxy :: Proxy Country))
+  , lawsToTest (QCC.enumLaws (Proxy :: Proxy Country))
+  ]
+
+lawsToTest :: QCC.Laws -> TestTree
+lawsToTest (QCC.Laws name pairs) = testGroup name (map (uncurry TQC.testProperty) pairs)
+
+instance QC.Arbitrary Country where
+  arbitrary = QC.arbitraryBoundedEnum
+
+
